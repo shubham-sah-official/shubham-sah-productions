32171e3b3b92c79af667c3bda6399365820b2483
Sah Shubham (BT - Customer Domain) <shubham.sah@hm.com>
2022-08-23 02:42:42 +0530

    Minor Change

--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -3,7 +3,7 @@ export const Team = (props) => {
     <div id='team' className='text-center'>
       <div className='container'>
         <div className='col-md-8 col-md-offset-2 section-title'>
-          <h2>Meet the Team</h2>
+          <h2>Meet the Production Team</h2>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
             dapibus leonec.
@@ -12,17 +12,17 @@ export const Team = (props) => {
         <div id='row'>
           {props.data
             ? props.data.map((d, i) => (
-                <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
-                  <div className='thumbnail'>
-                    {' '}
-                    <img src={d.img} alt='...' className='team-img' />
-                    <div className='caption'>
-                      <h4>{d.name}</h4>
-                      <p>{d.job}</p>
-                    </div>
+              <div key={`${d.name}-${i}`} className='col-md-3 col-sm-6 team'>
+                <div className='thumbnail'>
+                  {' '}
+                  <img src={d.img} alt='...' className='team-img' />
+                  <div className='caption'>
+                    <h4>{d.name}</h4>
+                    <p>{d.job}</p>
                   </div>
                 </div>
-              ))
+              </div>
+            ))
             : 'loading'}
         </div>
       </div>
